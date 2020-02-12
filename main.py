2b87141329f60b7de1e8bdf0d280aa401c083179
ivan <iv.fqu.ssh@gmail.com>
2020-02-12 19:11:23 +0200

    vlad

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return f'Ivan Shepetko TI-92'
 if __name__ == '__main__':
 	app.run('0.0.0.0')
 
