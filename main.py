10138582d32129245e0c1c9beccb369b25f2855f
ivan <iv.fqu.ssh@gmail.com>
2020-02-19 17:48:37 +0200

    wfwsd

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def hello():
     name = request.args.get("name", "World")
     return f'Ivan Shepetko TI-92'
+@app.route('/fisrt')
+def first():
+    num0 = None
+    num1 = 124
+    num2 = 123
+    name = request.args.get("name", "World")
+    return f'{num0}, {num2}, {num2}'
+
+
+
 if __name__ == '__main__':
 	app.run('0.0.0.0')
 
